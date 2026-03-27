aa5e4a85c6fb6ee29c26c01528ccdcd619b8988e
SukanyanShet <supremeautopartsad@gmail.com>
2026-03-28 03:35:08 +0530

    Replace fake logos with real client logos

--- src/components/companylogos.tsx
+++ src/components/companylogos.tsx
@@ -1,20 +1,18 @@
 import Image from 'next/image'
-import acmeLogo from "../assets/images/acme.png";
-import quantumLogo from "../assets/images/quantum.png";
-import echoLogo from "../assets/images/echo.png";
-import celestialLogo from "../assets/images/celestial.png";
-import pulseLogo from "../assets/images/pulse.png";
-import apexLogo from "../assets/images/apex.png";
+import aakashLogo from "../assets/images/aakash.png";
+import cultfitLogo from "../assets/images/culyfit.jpg";
+import hrxLogo from "../assets/images/hrx-logo-png.png";
+import lenskartLogo from "../assets/images/Lenskart.jpg";
+import titanLogo from "../assets/images/Titan.png";
 
 export default function LogoCarousel() {
 
   const logos = [
-  { src: acmeLogo, alt: "Acme Logo" },
-  { src: quantumLogo, alt: "Quantum Logo" },
-  { src: echoLogo, alt: "Echo Logo" },
-  { src: celestialLogo, alt: "Celestial Logo" },
-  { src: pulseLogo, alt: "Pulse Logo" },
-  { src: apexLogo, alt: "Apex Logo" },
+    { src: aakashLogo, alt: "Aakash Logo" },
+    { src: cultfitLogo, alt: "Cult.fit Logo" },
+    { src: hrxLogo, alt: "HRX Logo" },
+    { src: lenskartLogo, alt: "Lenskart Logo" },
+    { src: titanLogo, alt: "Titan Logo" },
   ]
 
   return (
@@ -22,14 +20,14 @@ export default function LogoCarousel() {
       <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
         {logos.map((logo, index) => (
           <li key={index}>
-            <Image src={logo.src} alt={logo.alt} />
+            <Image src={logo.src} alt={logo.alt} height={60} />
           </li>
         ))}
       </ul>
       <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll" aria-hidden="true">
         {logos.map((logo, index) => (
           <li key={index}>
-            <Image src={logo.src} alt={logo.alt} />
+            <Image src={logo.src} alt={logo.alt} height={60} />
           </li>
         ))}
       </ul>
