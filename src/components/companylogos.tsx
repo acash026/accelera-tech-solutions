6056f0c1ce70d551c4cb29a4297b96174fa4788a
SukanyanShet <supremeautopartsad@gmail.com>
2026-03-28 04:13:47 +0530

    Add real client logos

--- src/components/companylogos.tsx
+++ src/components/companylogos.tsx
@@ -1,18 +1,20 @@
 import Image from 'next/image'
 import aakashLogo from "../assets/images/aakash.png";
-import cultfitLogo from "../assets/images/culyfit.jpg";
-import hrxLogo from "../assets/images/hrx-logo-png.png";
-import lenskartLogo from "../assets/images/Lenskart.jpg";
+import cultfitLogo from "../assets/images/culyfit.png";
+import lenskartLogo from "../assets/images/Lenskart.png";
 import titanLogo from "../assets/images/Titan.png";
+import hclLogo from "../assets/images/hcltech.png";
+import hdfcLogo from "../assets/images/HDFC-Bank-logo.png";
 
 export default function LogoCarousel() {
 
   const logos = [
     { src: aakashLogo, alt: "Aakash Logo" },
     { src: cultfitLogo, alt: "Cult.fit Logo" },
-    { src: hrxLogo, alt: "HRX Logo" },
     { src: lenskartLogo, alt: "Lenskart Logo" },
     { src: titanLogo, alt: "Titan Logo" },
+    { src: hclLogo, alt: "HCLTech Logo" },
+    { src: hdfcLogo, alt: "HDFC Bank Logo" },
   ]
 
   return (
@@ -20,14 +22,14 @@ export default function LogoCarousel() {
       <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
         {logos.map((logo, index) => (
           <li key={index}>
-            <Image src={logo.src} alt={logo.alt} height={60} />
+            <Image src={logo.src} alt={logo.alt} height={60} style={{filter: "brightness(0) invert(1)"}} />
           </li>
         ))}
       </ul>
       <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll" aria-hidden="true">
         {logos.map((logo, index) => (
           <li key={index}>
-            <Image src={logo.src} alt={logo.alt} height={60} />
+            <Image src={logo.src} alt={logo.alt} height={60} style={{filter: "brightness(0) invert(1)"}} />
           </li>
         ))}
       </ul>
